ab1e2c2acef9fa26aaae04da3f1fd5bc8e535f6d
merrinx <gako.footwork856@passinbox.com>
2026-08-12 09:44:52 +0200

    feat(chat): add mapping for hnikt-tos-40067 to Vebjorn in UserMapping

--- client/src/chat/UserMapping.scala
+++ client/src/chat/UserMapping.scala
@@ -6,22 +6,23 @@ trait UserMapping:
 final class LiveUserMapping private () extends UserMapping:
   override def mapHostname(hostname: String): String =
     hostname match
-      case "terangreal"     => "Knut"
-      case "tuathaan"       => "Gako"
-      case "Solheim"        => "Jan-Magnus"
-      case "Turbonaepskrel" => "Magnus"
-      case "grindstein"     => "Torkil"
-      case "ievensen"       => "Harstad"
-      case "intervbs"       => "Joran"
-      case "jca"            => "Jan-Olov"
-      case "neethan"        => "Neethan"
-      case "neethan-hnikt"  => "Neethan"
-      case "sigubrat"       => "Sigurd"
-      case "williams"       => "Sigurd"
-      case "shitbox"        => "Leif"
-      case "ketil"          => "Ketil"
-      case "work"           => "Kristian"
-      case other            => other
+      case "terangreal"      => "Knut"
+      case "tuathaan"        => "Gako"
+      case "hnikt-tos-40067" => "Vebjorn"
+      case "Solheim"         => "Jan-Magnus"
+      case "Turbonaepskrel"  => "Magnus"
+      case "grindstein"      => "Torkil"
+      case "ievensen"        => "Harstad"
+      case "intervbs"        => "Joran"
+      case "jca"             => "Jan-Olov"
+      case "neethan"         => "Neethan"
+      case "neethan-hnikt"   => "Neethan"
+      case "sigubrat"        => "Sigurd"
+      case "williams"        => "Sigurd"
+      case "shitbox"         => "Leif"
+      case "ketil"           => "Ketil"
+      case "work"            => "Kristian"
+      case other             => other
 
 object LiveUserMapping:
   def apply(): UserMapping = new LiveUserMapping()
